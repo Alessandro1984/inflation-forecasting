5c2b36768266a54960adf8095d76a222f3a736ea
Stefanie Mossa <Stefanie.Mossa@posteo.de>
2023-03-09 18:14:21 +0100

    coding model.py, continue creating api.py, updating docker and requirements file

--- model.py
+++ model.py
@@ -1,80 +1,124 @@
-#Assumption: crossvalidation and optimization for hyperparameters here not needed, here we code the final models
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import GradientBoostingRegressor
+import seaborn as sns
+import glob
+from params import *
+#import time
 
-def get_data():
+
+##if function needed to select the right get data function?
+def get_data(country):
     '''
     Gets the data and converts it to a panda data frame. Does some first optimization for further work with the data
     '''
-    #To-Do: function to load the data frame from the raw data and do some first preprocossing
-    #add input parameters
-    #define what needs to be returned (assumed: "df" - panda dataframe)
+    # Check your current working directory using `os.getcwd()` below
+    os.getcwd()
 
-def preprocess_data():
-    '''
-    Preprocesses the data
-    '''
-    #To-To: write minimal preprocessing steps valid for all models in common
-    #To-DO: check if one model needs some individual additional prepprocessing steps and code it
-    #add input parameters
-    #define what needs to be returned (assumed: "X_preprocessed", "y_preprocessed")
+    #csv_path = os.path.join('..', 'data-context-and-setup', 'data', 'csv')
+    csv_path = os.path.join('..','raw_data')
 
-def train_test_split(X_preprocessed, y_preprocessed):
-    '''
-    Splits the already preprocessed data into train and test data
-    '''
-    #To-Do: create the folds
-    #To-Do: Define the train_test_ratio = 0.8 (as agreed in the project group)
-    #To-Do: do the split per fold
-    #Return "X_preprocessed_train", "y_preprocessed_train", "X_preprocessed_test", "y_preprocessed_test"
+    #pd.read_csv(os.path.join(csv_path, 'olist_sellers_dataset.csv')).head()
+    df = pd.read_csv(os.path.join(csv_path,'data_final.csv'))
 
+    # drop cpi column
+    df = df.drop('cpi',axis=1)
 
-def initialize_model(X_preprocessed_train, y_preprocessed_train):
+    #set time to time
+    df['year'] = pd.to_datetime(df['year'])
+    df = df.set_index('year')
+
+    #drop NaN
+    df = df.dropna(how='any')
+
+    df_country = df[df['country'] == country]
+
+    return df_country
+
+def defining_variables(df_country):
+    '''
+    Defines the variables for the  the already cleaned data
+    '''
+    X = df_country.drop(columns='core_CPI')
+    y = df_country['core_CPI']
+
+    return X, y
+
+def initialize_model():
     '''
     Initializes the model
     '''
-    #To-Do: check input parameters
-    #To-Do: select hyperparameters: regularizers etc.
-    #To-Do: initialize the sequential model
-    #To-Do: add layers to the model
-    #To-Do: return the "model"
+    # Define the best hyperparameters from the grid search
+    best_learning_rate = 0.1
+    best_n_estimators = 100
+    best_max_depth = 3
+    best_max_features = 10 # reduced number of features used for each split
+    best_alpha = 0.001
 
-def compile_model(model):
-    '''
-    Compiles the model
-    '''
-    #To-Do: check input parameters
-    #To-Do: select hyperparameters: normalizers etc.
-    #To-DO: select metrics "mse" and loss "mse" (as agreed in the project group)
+    # Create a new GradientBoostingRegressor with the best hyperparameters
+    model = GradientBoostingRegressor(
+    learning_rate=best_learning_rate,
+    n_estimators=best_n_estimators,
+    max_depth=best_max_depth,
+    max_features=best_max_features,
+     alpha=best_alpha
+    )
 
-    #To-Do: return the model
+    return model
 
-def train_model(model):
+def train_model(model, X, y):
     '''
     Trains the compiled model
     '''
-    #To-Do: check input parameters
-    #To-Do: select metrics,learning_rate, patience, batch_size, validation_split
-    #To-Do: returns the "model" and the "history" (history is needed for further plotting in the frontend!)
-    #To-Do: returns -some how - "actual_inflation"
 
-def predict_model(model):
+    # Fit the GradientBoostingRegressor to the training data
+    trained_model = model.fit(X, y)
+    return trained_model
+
+
+def predict_model(trained_model, X):
     '''
     Predicts for the trained model
     '''
-    #To-Do: check input parameters
-    #To-Do: Returns - some how - the "predicted_inflation"
+    # Make predictions on the validation set
+    y_pred = trained_model.predict(X)
+    return y_pred
 
-def save_model(model):
+def save_y_pred(y_pred, country):
     '''
-    Persist trained model in GC"
+    Persist predicted targets locally
     '''
-    #To-Do: Choose the location where to save the model (assumed: GC)
-    #To-Do: Define GC client, bucket and blob
-    #To-Do: "None" to be returned
+    #timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    # save y_pred locally resp. in the container's filesystem
+    #@TA; how to get the filepath in the docker container
+    saved_prediction_path =  LOCAL_FILE_PATH
+    y_pred = y_pred.save(saved_prediction_path)
 
 
-def load_model(model):
+def load_y_pred(country):
     '''
-    Return a saved model (most current version)
+    Return saved predicted y_pred
     '''
-    #To-Do: define blob, client, latest blob, latest client, latest model
-    #To-Do: return "latest_model"
+    local_prediction_directory = LOCAL_FILE_PATH
+    #local_prediction_path = glob.glob(f"{local_prediction_directory}/*")
+    with open(local_prediction_directory/f"{country}.h5", "r") as function:
+        y_pred = function.read()
+    return y_pred
+
+    #most_recent_model_path_on_disk = sorted(local_model_paths)[-1]
+    #print(Fore.BLUE + f"\nLoad latest model from disk..." + Style.RESET_ALL)
+    #lastest_model = keras.models.load_model(most_recent_model_path_on_disk)
+
+if __name__ == "__main__":
+    df = get_data()
+    defining_variables(df)
+    model = initialize_model()
+    train_model()
+    predict_model()
+    save_y_pred()
+    load_y_pred()
