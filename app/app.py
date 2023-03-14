f41cd326dbbd4ebd71897149b8e16fee264c06a1
Alessandro Bramucci <bramucci.alessandro@gmail.com>
2023-03-14 14:21:10 +0100

    App and api work

--- app/app.py
+++ app/app.py
@@ -7,19 +7,13 @@ import os
 import requests
 import json
 
+import streamlit as st
+# from sessionstate import SessionState # import SessionState
+# https://stackoverflow.com/questions/63988485/modulenotfounderror-no-module-named-sessionstate
+
 #urlAPI = "http://127.0.0.1:8000/predict"
 #urlAPItest = "http://127.0.0.1:8000/test"
 urlAPI = "http://127.0.0.1:8000"
-
-# dict_xxx = {
-#   "predictions": "[[5.426177978515625], [5.317831993103027], [5.216428756713867], [5.11897087097168], [5.024357795715332], [4.9318342208862305], [4.84096097946167], [4.751753807067871], [4.6642231941223145], [4.57786750793457], [4.492310047149658], [4.407289981842041], [4.322705268859863], [4.238522052764893], [4.154750347137451], [4.071430683135986], [3.9886269569396973], [3.9064197540283203], [3.824902296066284], [3.744175910949707], [3.6643474102020264], [3.58552622795105], [3.507821798324585], [3.431339979171753]]"
-# }
-
-# predictions = json.loads(dict_xxx['predictions'])
-
-# predictions = [[float(x[0])] for x in predictions]  # wrap each value in a list to create a 2D array
-
-# predictions = np.array(predictions)
 
 st.markdown("""# use deep learning to predict inflation!
 ## use the predict button below""")
@@ -68,86 +62,120 @@ with st.sidebar:
     inflation_type = st.radio("Inflation Type", ('Headline Inflation', 'Core Inflation'))
     st.markdown("<div class='widget-box'></div>", unsafe_allow_html=True)
     # Slider month
-    num_months = st.slider('Number of Months to Predict', 1, 48)
+    num_months = st.slider('Number of Months to Predict', 1, 24)
 
 if inflation_type == 'Headline Inflation':
     inflation_type = 'cpi'
 else:
     inflation_type = 'ccpi'
 
-df_country = df[df['country'] == country]
+df_country = df[df['country_id'] == country]
 
-# fig = plot_function(data_country = df_country,
-#                     data_forecast_country = predictions,
-#                     user_input = predictions.shape[0])
+#if st.button("predict"):
 
-# st.plotly_chart(fig,
-#                 use_container_width=True)
+# payload = {'num_months': num_months}
+payload = {'country': country,
+            'inflation_type': inflation_type,
+            'num_months': num_months}
 
-if st.button("predict"):
+# response_model = requests.post(f"{urlAPI}/model",
+#                                data = payload)
 
-    # payload = {'num_months': num_months}
-    payload = {'country': country,
-               'inflation_type': inflation_type,
-               'num_months': num_months}
+file_csv = os.path.join(csv_path,'data_final.csv')
+data_df = pd.read_csv(file_csv, index_col=0)
+# print(data_df.head())
+data_df = data_df[data_df['country_id'] == country]
+print(data_df.head())
+data_df = data_df[inflation_type] # Be carefull here!
+print(data_df.head())
+df = pd.DataFrame(data_df, dtype="f2")
+df_byte = df.to_json().encode()
+response = requests.post(f"{urlAPI}/predict",
+                            params= payload,
+                            files = {"test_file": df_byte})
+prediction = response.json()["predictions"]
+# response = requests.post(f"{urlAPI}/predict_test",
+                        # files = {"test_file": df_byte})
+# if response.ok:
+#     prediction = response.json()["predictions"]
+#     st.success("Prediction successful!")
+#     # Display prediction
+#     st.write(prediction)
+# else:
+#     st.error("Prediction failed.")
 
-    # response_model = requests.post(f"{urlAPI}/model",
-    #                                data = payload)
+# PLOT
+fig = go.Figure()
 
-    file_csv = os.path.join(csv_path,'data_final.csv')
-    data_df = pd.read_csv(file_csv, index_col=0)
-    # print(data_df.head())
-    data_df = data_df[data_df['country_id'] == country]
-    print(data_df.head())
-    data_df = data_df[inflation_type] # Be carefull here!
-    print(data_df.head())
-    df = pd.DataFrame(data_df, dtype="f2")
-    df_byte = df.to_json().encode()
-    response = requests.post(f"{urlAPI}/predict",
-                             params= payload,
-                             files = {"test_file": df_byte})
-    # response = requests.post(f"{urlAPI}/predict_test",
-                            # files = {"test_file": df_byte})
-    if response.ok:
-        prediction = response.json()["predictions"]
-        st.success("Prediction successful!")
-        # Display prediction
-        st.write(prediction)
-    else:
-        st.error("Prediction failed.")
+df_short = df_country.loc[df_country["year"] >= "2015-01-01"]
+first_date = df_short['year'].min()
+last_date = pd.to_datetime(df_short['year'].max()) + pd.offsets.MonthBegin(1)
+#country_name = df_short["country"].unique()[0]
+t = pd.date_range(last_date, periods = num_months, freq='MS')
+forecast_date = t.max()
+out_forecast_df = pd.DataFrame([[x, y] for x, y in zip(t, np.array(prediction).reshape(-1, 1))], columns=["year", "Forecast"])
+out_forecast_df['Forecast'] = out_forecast_df['Forecast'].apply(lambda x: np.ravel(x)[0])
 
+fig.add_trace(go.Scatter(x = df_short['year'],
+                         y = df_short[inflation_type],
+                         line_color = "blue",
+                         name = "Inflation",
+                         mode = "lines"))
 
-# Try to convert all variables in df_final in type dtype="u1" and then send the file to the API
-# Convert dict to dataframe in fast_api
+fig.add_trace(go.Scatter(x = out_forecast_df["year"],
+                         y = out_forecast_df['Forecast'],
+                         name = "Forecasted Core CPI",
+                         line_dash = "dash",
+                         line_color = "black",
+                         mode = "lines"))
 
-# if st.button("Predict"):
+fig.add_vrect(x0 = last_date,
+              x1 = forecast_date,
+              fillcolor = "grey",
+              opacity = 0.25,
+              line_width = 0)
 
-#     file_csv = os.path.join(csv_path,'data_final.csv')
-#     data_df = pd.read_csv(file_csv, index_col=0)
-#     data_df = data_df[data_df['country_id'] == country]
-#     data_df = data_df.to_json()
-#     response = requests.post(urlAPI, files = {"csv_file": data_df})
-#     print(response)
+fig.update_layout(
+        title = f"Out-of-sample forecast until {forecast_date.strftime('%B %Y')}",
+        xaxis_title = "",
+        yaxis_title = "Monthly y-o-y percentage change",
+        autosize=False,
+        hoverlabel_namelength=-1,
+        width=890,
+        height=600,
+      legend = dict(
+            xanchor = "center",
+            yanchor = "top",
+            y = -0.2,
+            x = 0.5,
+            orientation = 'h'
+      ),
+      xaxis=dict(
+          dtick='M12',
+          tickangle=45,
+          tickfont=dict(size=14)
+        ),
+      yaxis=dict(
+          tickfont=dict(size=14)
+        )
+    )
 
-st.markdown("""# To do:
-        - How to return the whole series of future forecasts and not just the first?
-        - The country is hardcoded (default 'USA')
-        - The target is hardcoded (default 'ccpi')
-        - Number of future months forecasts is hardcoded (dafault 24)
-            """)
+fig.update_layout(
+plot_bgcolor='white')
 
-   # st.spinner("Waiting for prediction...")
+fig.update_xaxes(
+    mirror=True,
+    ticks='outside',
+    showline=True,
+    linecolor='black',
+    gridcolor='lightgrey'
+)
+fig.update_yaxes(
+    mirror=True,
+    ticks='outside',
+    showline=True,
+    linecolor='black',
+    gridcolor='lightgrey')
 
-    #print(os.path.join(csv_path,'data_final.csv'))
-
-    # uploaded_file =st.file_uploader(
-        # "upload your file", type=["csv"],
-        # accept_multiple_files=False)
-    #print(df.info())
-    #st.write(bytes_csv)
-    #df_byte = df.to_json()
-    #print(df_byte)
-    #response = requests.post(urlAPI, files = {"csv_file": df_byte})
-    #result = response.json()["predictions"]
-    #print(response.json())
-    #st.write("Done")
+st.plotly_chart(fig,
+                use_container_width=True)
